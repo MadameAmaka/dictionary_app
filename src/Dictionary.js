36a0e75b68d84b7914adf039c151a43d7ddf80cf
Amaka Onwuli <amonwuli@gmail.com>
2025-05-03 21:13:36 +0100

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,21 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
+
+
+    function handleResponse(response){
+        
+    }
+
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`)
+        alert(`Searching for ${keyword}`);
+
+        let apiKey = "2f43379oac5f7ffe8tde5aff442f0cdb";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
 
     }
     
